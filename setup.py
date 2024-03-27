3a2fe0526a9a3b061251e23058baae297edda5aa
Sam Knisely <sck4jh@virginia.edu>
2024-03-26 20:23:54 -0400

    Participation Module 9

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='My_Package',
+    versions = '0.1',
+    description = 'description',
+    url = 'https://github.com/Sam-Knisely/DS5100-P',
+    author = 'Sam',
+    license = 'MIT',
+    packages=['my_package']
+)
